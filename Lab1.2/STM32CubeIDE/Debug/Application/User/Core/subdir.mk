95c0326a7285fcb1b9cceb6064a47862960299e9
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2025-07-23 13:19:33 +0700

    lab1 done, lab2 wip

---
+++ Lab1.2/STM32CubeIDE/Debug/Application/User/Core/subdir.mk
@@ -0,0 +1,45 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (13.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+/home/phuwit/Programming/KMITL-Microcontroller/Lab1.2/Core/Src/main.c \
+/home/phuwit/Programming/KMITL-Microcontroller/Lab1.2/Core/Src/stm32f7xx_hal_msp.c \
+/home/phuwit/Programming/KMITL-Microcontroller/Lab1.2/Core/Src/stm32f7xx_it.c \
+../Application/User/Core/syscalls.c \
+../Application/User/Core/sysmem.c 
+
+OBJS += \
+./Application/User/Core/main.o \
+./Application/User/Core/stm32f7xx_hal_msp.o \
+./Application/User/Core/stm32f7xx_it.o \
+./Application/User/Core/syscalls.o \
+./Application/User/Core/sysmem.o 
+
+C_DEPS += \
+./Application/User/Core/main.d \
+./Application/User/Core/stm32f7xx_hal_msp.d \
+./Application/User/Core/stm32f7xx_it.d \
+./Application/User/Core/syscalls.d \
+./Application/User/Core/sysmem.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Application/User/Core/main.o: /home/phuwit/Programming/KMITL-Microcontroller/Lab1.2/Core/Src/main.c Application/User/Core/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m7 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F767xx -c -I../../Core/Inc -I../../Drivers/STM32F7xx_HAL_Driver/Inc -I../../Drivers/STM32F7xx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32F7xx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv5-d16 -mfloat-abi=hard -mthumb -o "$@"
+Application/User/Core/stm32f7xx_hal_msp.o: /home/phuwit/Programming/KMITL-Microcontroller/Lab1.2/Core/Src/stm32f7xx_hal_msp.c Application/User/Core/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m7 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F767xx -c -I../../Core/Inc -I../../Drivers/STM32F7xx_HAL_Driver/Inc -I../../Drivers/STM32F7xx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32F7xx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv5-d16 -mfloat-abi=hard -mthumb -o "$@"
+Application/User/Core/stm32f7xx_it.o: /home/phuwit/Programming/KMITL-Microcontroller/Lab1.2/Core/Src/stm32f7xx_it.c Application/User/Core/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m7 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F767xx -c -I../../Core/Inc -I../../Drivers/STM32F7xx_HAL_Driver/Inc -I../../Drivers/STM32F7xx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32F7xx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv5-d16 -mfloat-abi=hard -mthumb -o "$@"
+Application/User/Core/%.o Application/User/Core/%.su Application/User/Core/%.cyclo: ../Application/User/Core/%.c Application/User/Core/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m7 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F767xx -c -I../../Core/Inc -I../../Drivers/STM32F7xx_HAL_Driver/Inc -I../../Drivers/STM32F7xx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32F7xx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv5-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Application-2f-User-2f-Core
+
+clean-Application-2f-User-2f-Core:
+	-$(RM) ./Application/User/Core/main.cyclo ./Application/User/Core/main.d ./Application/User/Core/main.o ./Application/User/Core/main.su ./Application/User/Core/stm32f7xx_hal_msp.cyclo ./Application/User/Core/stm32f7xx_hal_msp.d ./Application/User/Core/stm32f7xx_hal_msp.o ./Application/User/Core/stm32f7xx_hal_msp.su ./Application/User/Core/stm32f7xx_it.cyclo ./Application/User/Core/stm32f7xx_it.d ./Application/User/Core/stm32f7xx_it.o ./Application/User/Core/stm32f7xx_it.su ./Application/User/Core/syscalls.cyclo ./Application/User/Core/syscalls.d ./Application/User/Core/syscalls.o ./Application/User/Core/syscalls.su ./Application/User/Core/sysmem.cyclo ./Application/User/Core/sysmem.d ./Application/User/Core/sysmem.o ./Application/User/Core/sysmem.su
+
+.PHONY: clean-Application-2f-User-2f-Core
+
---
+++ Lab1.2/STM32CubeIDE/Debug/Drivers/CMSIS/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (13.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+/home/phuwit/Programming/KMITL-Microcontroller/Lab1.2/Core/Src/system_stm32f7xx.c 
+
+OBJS += \
+./Drivers/CMSIS/system_stm32f7xx.o 
+
+C_DEPS += \
+./Drivers/CMSIS/system_stm32f7xx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Drivers/CMSIS/system_stm32f7xx.o: /home/phuwit/Programming/KMITL-Microcontroller/Lab1.2/Core/Src/system_stm32f7xx.c Drivers/CMSIS/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m7 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F767xx -c -I../../Core/Inc -I../../Drivers/STM32F7xx_HAL_Driver/Inc -I../../Drivers/STM32F7xx_HAL_Driver/Inc/Legacy -I../../Drivers/CMSIS/Device/ST/STM32F7xx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv5-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Drivers-2f-CMSIS
+
+clean-Drivers-2f-CMSIS:
+	-$(RM) ./Drivers/CMSIS/system_stm32f7xx.cyclo ./Drivers/CMSIS/system_stm32f7xx.d ./Drivers/CMSIS/system_stm32f7xx.o ./Drivers/CMSIS/system_stm32f7xx.su
+
+.PHONY: clean-Drivers-2f-CMSIS
+
