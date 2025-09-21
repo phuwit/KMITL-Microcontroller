475f21c6d4ea2fd752ec298fd9ecf759591df0bb
Phuwit Puthipairoj <26784267+phuwit@users.noreply.github.com>
2025-09-21 16:24:39 +0700

    optimize flickering/rendering and added not working pwm brightness

--- Lab8-Special/Core/Inc/main.h
+++ Lab8-Special/Core/Inc/main.h
@@ -52,6 +52,8 @@ extern uint8_t screen;
 
 /* USER CODE END EM */
 
+void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
+
 /* Exported functions prototypes ---------------------------------------------*/
 void Error_Handler(void);
 
